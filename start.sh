ddb1ffcf2664e6bdc865fe22658bc769818a1e5f
Andrew <sokol.fokir@gmail.com>
2021-11-04 18:34:09 +0400

    Create shell script for update before every run app

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+git remote update
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+   echo "Latest files in repository"
+elif [ $LOCAL = $BASE ]; then
+   echo 'Start updated files from git'
+   git pull
+   npm install
+   npm run build
+elif [ $REMOTE = $BASE ]; then
+    echo "Local files has been changed!"
+else
+    echo "Unknown status"
+fi
+
+npm run start
