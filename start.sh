70e7f0d33a9290501b67f597b4124ef5722f0127
Andrew <sokol.fokir@gmail.com>
2021-11-05 18:19:29 +0400

    Light control with a button

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
+export NODE_ENV="production"
+
 if [ $LOCAL = $REMOTE ]; then
    echo "Latest files in repository"
 elif [ $LOCAL = $BASE ]; then
@@ -20,5 +22,4 @@ else
     echo "Unknown status"
 fi
 
-export NODE_ENV="production"
 npm run start
